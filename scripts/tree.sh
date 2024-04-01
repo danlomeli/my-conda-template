ef66e33bec8c57db897aa4ecaae764000eb1b8e6
Daniel Lomeli <dan.lomeli@gmail.com>
2024-04-01 11:47:42 +0700

    rm forward slash in tree script

--- scripts/tree.sh
+++ scripts/tree.sh
@@ -3,13 +3,13 @@
 ignorefile=.gitignore
 
 # Check if .gitignore file exists
-if [ ! -f $ignorefile ]; then
+if [ ! -f "$ignorefile" ]; then
   echo "Error: .gitignore file not found in the current directory."
   exit 1
 fi
 
 # Read the .gitignore file and generate the exclude pattern
-exclude_pattern=$(grep -v '^#' $ignorefile | grep -v '^$' | tr '\n' '|' | sed 's/|$//')
+exclude_pattern=$(grep -v '^#' "$ignorefile" | grep -v '^$' | sed 's:/$::' | tr '\n' '|' | sed 's/|$//')
 
 # Run the tree command with the exclude pattern
 tree -a -I "$exclude_pattern"
