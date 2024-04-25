c7f1ee6fdcf2a25ddeb5593efb271068163eeaca
Daniel Lomeli <dan.lomeli@gmail.com>
2024-04-25 10:50:36 +0700

    Modifying cat script to ignore binary files

--- scripts/cat.sh
+++ scripts/cat.sh
@@ -2,8 +2,8 @@
 
 # Check if .gitignore file exists
 if [ ! -f ".gitignore" ]; then
-  echo "Error: .gitignore file not found in the current directory."
-  exit 1
+    echo "Error: .gitignore file not found in the current directory."
+    exit 1
 fi
 
 # Read the .gitignore file and generate the exclude pattern
@@ -14,6 +14,9 @@ shopt -s nullglob dotglob
 
 # Find all files in the current directory and its subdirectories, excluding files and directories from .gitignore
 while IFS= read -r -d '' file; do
-  echo -e "\n**********\nFilename: $file\nContent:\n"
-  cat "$file"
+    # Check if the file is a regular file and not a binary file
+    if [ -f "$file" ] && ! file --mime "$file" | grep -q 'binary'; then
+        echo -e "\n********\nFilename: $file\nContent:\n"
+        cat "$file"
+    fi
 done < <(find . -type f -print0 | grep -zZvE "$exclude_pattern")
